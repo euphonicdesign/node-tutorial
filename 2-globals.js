4c2a7e149faa47b0a458e1e3100489afde9de6bb
euphonicdesign <25894355+euphonicdesign@users.noreply.github.com>
2023-01-17 05:04:50 +0200

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,14 @@
+// GLOBALS - NO WINDOW !
+
+//__dirname     
+//__filename    
+//require       
+//module
+//process
+
+console.log(__dirname);
+console.log(__filename);
+
+setInterval(()=>{
+    console.log("Hello World!");
+}, 1000);
---
+++ app.js
@@ -0,0 +1,11 @@
+// npm --version
+// npm i <package name>
+// npm install -g <packageName>
+// npm init
+// npm init -y
+
+const _ = require('lodash');
+
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
